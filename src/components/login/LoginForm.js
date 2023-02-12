d2e374e373acc826b2714909cf6fd488f01de77d
ruyisbaros <ahmet.erdonmez77@gmail.com>
2023-02-12 18:50:04 +0100

    login done

---
+++ src/components/login/LoginForm.js
@@ -0,0 +1,72 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom';
+import logo from "../../assets/output-onlinejpgtools (1).png"
+import { Formik, Form } from 'formik'
+import * as Yup from "yup"
+import LoginInput from './../inputs/loginInput/LoginInput';
+
+const LoginForm = () => {
+    const [login, setLogin] = useState({ email: "", password: "" })
+    const { email, password } = login;
+    console.log(login);
+    const handleLoginChange = (e) => {
+        const { name, value } = e.target;
+        setLogin({ ...login, [name]: value });
+    };
+    const loginValidation = Yup.object({
+        email: Yup.string().required("Email address is required").email("Must be valid email"),
+        password: Yup.string().required("Password is required").min(6)
+    })
+    return (
+        <div className="login_wrap">
+            <div className="login_1">
+                <img src={logo} alt="" />
+                <span>iNeedSomething helps you connect and share with the people in your life.</span>
+            </div>
+            <div className="login_2">
+                <div className="login_2_wrap">
+                    <Formik
+                        enableReinitialize
+                        initialValues={{
+                            email,
+                            password
+                        }}
+                        validationSchema={loginValidation}
+                    >
+                        {
+                            (formik) => (
+                                <Form>
+                                    <LoginInput
+                                        placeholder="Email address or phone number"
+                                        type="text"
+                                        name="email"
+                                        onChange={handleLoginChange}
+                                        bottom={false}
+                                    />
+                                    <LoginInput
+                                        placeholder="Password"
+                                        type="password"
+                                        name="password"
+                                        onChange={handleLoginChange}
+                                        bottom={true}
+                                    />
+                                    <button type='submit' className='blue_btn'>Log In</button>
+                                </Form>
+                            )
+                        }
+                    </Formik>
+                    <Link to="/forgot_pwd" className='forgot_password' >Forgotten password ?</Link>
+                    <div className="sign_splitter"></div>
+                    <button className="blue_btn open_signup">Create Account</button>
+
+                </div>
+                <Link to="/" className='sign_extra'>
+                    <b>Create a Page </b>
+                    for a celebrity, brand or business.
+                </Link>
+            </div>
+        </div>
+    )
+}
+
+export default LoginForm
--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,75 +1,75 @@
-import React, { useState } from 'react'
-import logo from "../../assets/output-onlinejpgtools (1).png"
-import { Formik, Form } from 'formik'
+import React from 'react'
 import { Link } from 'react-router-dom'
-import LoginInput from '../../components/inputs/loginInput/LoginInput'
-import * as Yup from "yup"
+import { AiOutlinePlus } from "react-icons/ai"
+import { MdAddToDrive } from "react-icons/md"
 import "./login.css"
+import LoginForm from '../../components/login/LoginForm'
 
 const Login = () => {
-    const [login, setLogin] = useState({ email: "", password: "" })
-    const { email, password } = login;
-    console.log(login);
-    const handleLoginChange = (e) => {
-        const { name, value } = e.target;
-        setLogin({ ...login, [name]: value });
-    };
-    const loginValidation = Yup.object({
-        email: Yup.string().required("Email address is required").email("Must be valid email"),
-        password: Yup.string().required("Password is required").min(6)
-    })
+
     return (
         <div className='login'>
             <div className="login_wrapper">
-                <div className="login_wrap">
-                    <div className="login_1">
-                        <img src={logo} alt="" />
-                        <span>iNeedSomething helps you connect and share with the people in your life.</span>
-                    </div>
-                    <div className="login_2">
-                        <div className="login_2_wrap">
-                            <Formik
-                                enableReinitialize
-                                initialValues={{
-                                    email,
-                                    password
-                                }}
-                                validationSchema={loginValidation}
-                            >
-                                {
-                                    (formik) => (
-                                        <Form>
-                                            <LoginInput
-                                                placeholder="Email address or phone number"
-                                                type="text"
-                                                name="email"
-                                                onChange={handleLoginChange}
-                                                bottom={false}
-                                            />
-                                            <LoginInput
-                                                placeholder="Password"
-                                                type="password"
-                                                name="password"
-                                                onChange={handleLoginChange}
-                                                bottom={true}
-                                            />
-                                            <button type='submit' className='blue_btn'>Log In</button>
-                                        </Form>
-                                    )
-                                }
-                            </Formik>
-                            <Link to="/forgot_pwd" className='forgot_password' >Forgotten password ?</Link>
-                            <div className="sign_splitter"></div>
-                            <button className="blue_btn open_signup">Create Account</button>
-
+                <LoginForm />
+                <div className="register">
+                    <footer className='login_footer'>
+                        <div className="login_footer_wrap">
+                            <Link to="/">English(UK)</Link>
+                            <Link to="/">Français(FR)</Link>
+                            <Link to="/">العربية</Link>
+                            <Link to="/">ⵜⴰⵎⴰⵣⵉⵖⵜ</Link>
+                            <Link to="/">Español (España)</Link>
+                            <Link to="/">italiano</Link>
+                            <Link to="/">Deutsch</Link>
+                            <Link to="/">Português (Brasil)</Link>
+                            <Link to="/">हिन्दी</Link>
+                            <Link to="/">中文(简体)</Link>
+                            <Link to="/">日本語</Link>
+                            <Link to="/" className="footer_square">
+                                <AiOutlinePlus className='footer_square_icon' size={15} />
+                            </Link>
                         </div>
-                        <Link to="/" className='sign_extra'>
-                            <b>Create a Page </b>
-                            for a celebrity, brand or business.
-                        </Link>
-                    </div>
+                        <div className="footer_splitter"></div>
+                        <div className="login_footer_wrap">
+                            <Link to="/">Sign Up</Link>
+                            <Link to="/">Log in</Link>
+                            <Link to="/">Messenger</Link>
+                            <Link to="/">Facebook Lite</Link>
+                            <Link to="/">Watch</Link>
+                            <Link to="/">Places</Link>
+                            <Link to="/">Games</Link>
+                            <Link to="/">Marketplace</Link>
+                            <Link to="/">Facebook Pay</Link>
+                            <Link to="/">Oculus</Link>
+                            <Link to="/">Portal</Link>
+                            <Link to="/">Instagram</Link>
+                            <Link to="/">Bulletin</Link>
+                            <Link to="/">Local</Link>
+                            <Link to="/">Fundraisers</Link>
+                            <Link to="/">Services</Link>
+                            <Link to="/">Voting Information Centre</Link>
+                            <Link to="/">Groups</Link>
+                            <Link to="/">About</Link>
+                            <Link to="/">Create ad</Link>
+                            <Link to="/">Create Page</Link>
+                            <Link to="/">Developers</Link>
+                            <Link to="/">Careers</Link>
+                            <Link to="/">Privacy</Link>
+                            <Link to="/">Cookies</Link>
+                            <Link to="/">
+                                AddChoices
+                                <MdAddToDrive size={20} />
+                            </Link>
+                            <Link to="/">Terms</Link>
+                            <Link to="/">Help</Link>
+                        </div>
+                        <div className="login_footer_wrap">
+                            <Link to="/" style={{ fontSize: "12px", marginTop: "10px" }}>
+                                Ahmet © 2022
+                            </Link>
+                        </div>
+                    </footer>
                 </div>
-                <div className="register_wrap"></div>
             </div>
         </div>
     )

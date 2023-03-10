150c81dad9dccd7c77d17d69219663b3dd2be7bc
ruyisbaros <ahmet.erdonmez77@gmail.com>
2023-03-10 23:18:18 +0100

    remove from search history

--- src/components/header/SearchMenu.js
+++ src/components/header/SearchMenu.js
@@ -1,6 +1,8 @@
 import React, { useRef, useState, useEffect, useCallback } from 'react'
+import { Link } from 'react-router-dom';
 import { toast } from 'react-toastify';
-import { searchUsersRegex, useDebounce } from '../../services/SearchService';
+import { addSearchHistory, deleteFromSearchHistory, getSearchHistory, searchUsersRegex, useDebounce } from '../../services/SearchService';
+import { PacmanLoader } from "react-spinners"
 import { Return, Search } from '../../svg'
 import { useOutsideClick } from './../../utils/helpers';
 
@@ -10,6 +12,7 @@ const SearchMenu = ({ setShowSearchMenu, showSearchMenu }) => {
     const [loading, setLoading] = useState(false)
     const [search, setSearch] = useState("")
     const [searchResults, setSearchResults] = useState([])
+    const [history, setHistory] = useState([])
     const menuRef = useRef(null)
     const inputRef = useRef(null)
     const debouncedValue = useDebounce(search, 1000) //make soft search
@@ -28,8 +31,8 @@ const SearchMenu = ({ setShowSearchMenu, showSearchMenu }) => {
             if (query) {
                 const res = await searchUsersRegex(query, setLoading)
                 setSearchResults(res)
-                console.log(res)
-            }
+                //console.log(res)
+            } 
         } catch (error) {
             toast.error(error.response.data.message)
         }
@@ -38,8 +41,28 @@ const SearchMenu = ({ setShowSearchMenu, showSearchMenu }) => {
     useEffect(() => {
         if (debouncedValue) {
             searchUserHandler(debouncedValue)
+        } else {
+            setSearchResults([])
         }
     }, [searchUserHandler, debouncedValue])
+
+    const handleAddSearchHistory = async (id) => {
+        await addSearchHistory(id)
+        setShowSearchMenu(false)
+    }
+    const handleRemoveFromSearchHistory = async (id) => {
+        await deleteFromSearchHistory(id)
+        setHistory(history.filter(item => item.user._id !== id))
+    }
+
+    const handleGetSearchHistory = useCallback(async () => {
+        const res = await getSearchHistory()
+        //console.log(res)
+        setHistory(res)
+    }, [])
+    useEffect(() => {
+        handleGetSearchHistory()
+    }, [handleGetSearchHistory])
 
     return (
         <div className='header_left search_area scrollbar' ref={menuRef}>
@@ -66,14 +89,45 @@ const SearchMenu = ({ setShowSearchMenu, showSearchMenu }) => {
                     />
                 </div>
             </div>
-            <div className="search_history_header">
-                <span>Recent searches</span>
-                <a href="#">Edit</a>
-            </div>
-            <div className="search_history">
+            {search === "" &&
+                <>
+                <div className="search_history_header">
+                    <span>Recent searches</span>
+                    <a href="#">Edit</a>
+                </div>
+                    <div className="search_history scrollbar">
+                        {history.length > 0 &&
+                            history
+                                .sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
+                                .map(src => (
+                                    <div key={src.user.username} className="search_user_item hover1">
+                                        <Link className='flex' to={`/profile/${src.user.username}`}
+                                            onClick={() => handleAddSearchHistory(src.user?._id)}>
+                                            <img src={src.user?.picture} alt="" />
+                                            <span>{src.user?.first_name}{" "}{src.user?.last_name}</span>
+                                        </Link>
+                                        <i onClick={() => handleRemoveFromSearchHistory(src.user?._id)} className="exit_icon"></i>
+                                    </div>
+                                ))
+                        }
+                    </div>
+                </>
+            }
+            <div className="search_results scrollbar">
+                <div className='search_loader'>
+                    <PacmanLoader size={15} color='#65676b'
+                        loading={loading} />
+                </div>
 
+                {searchResults.length > 0 &&
+                    searchResults.map(src => (
+                        <Link key={src?.email} to={`/profile/${src.username}`} className="search_user_item hover1"
+                            onClick={() => handleAddSearchHistory(src._id)}>
+                            <img src={src?.picture} alt="" />
+                            <span>{src?.first_name}{" "}{src?.last_name}</span>
+                        </Link>
+                    ))}
             </div>
-            <div className="search_results scrollbar"></div>
         </div>
     )
 }
--- src/services/SearchService.js
+++ src/services/SearchService.js
@@ -18,7 +18,7 @@ export const useDebounce = (value, delay) => {
 
 export const searchUsersRegex = async (search, setLoading) => {
     try {
-        setLoading(false)
+        setLoading(true)
         const { data } = await axios.get(`/users/search_users/${search}`)
         setLoading(false)
         return data?.searchedUsers
@@ -27,3 +27,28 @@ export const searchUsersRegex = async (search, setLoading) => {
         toast.error(error.response.data.message)
     }
 }
+export const addSearchHistory = async (id) => {
+    try {
+        await axios.patch(`/users/add_search_history/${id}`, {})
+
+    } catch (error) {
+        toast.error(error.response.data.message)
+    }
+}
+export const getSearchHistory = async () => {
+    try {
+        const { data } = await axios.get(`/users/get_search_history`,)
+        return data?.history
+    } catch (error) {
+        toast.error(error.response.data.message)
+    }
+}
+export const deleteFromSearchHistory = async (id) => {
+    try {
+        const { data } = await axios.patch(`/users/delete_search_history/${id}`, {})
+        return data?.history
+    } catch (error) {
+        toast.error(error.response.data.message)
+    }
+}
+
